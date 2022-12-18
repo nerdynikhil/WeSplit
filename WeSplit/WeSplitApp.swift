8245b01a7e8c19318e55b3e839d759b9bbd6d703
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2022-12-18 10:49:08 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nikhil Barik on 18/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
